31d55fae62d0a4aee478ad1bfe606c5515e3dfc3
bma22 <maxim.baetens@telenet.be>
2014-11-18 22:51:13 +0100

    Update cachematrix.R
    Solution to assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below calculate the inverse of a matrix, making use of cached data.
 
-## Write a short comment describing this function
+## The makeCachematrix-function sets the value in setmax and gets the inverse value in getmat
+## It stores all data in a list.
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    # Original matrix
+    x <<- y
+    # Inverse matix
+    i <<- NULL
+  }
+  get <- function() x
+  setmat <- function(solve) i <<- solve
+  getmat <- function() i
+  list(set = set, get = get,
+       setmat = setmat,
+       getmat = getmat)
 
 }
 
 
-## Write a short comment describing this function
+## This function checks whether there is already cached data.
+## If this is the case, it retrieves the cached data.
+## If not, it stores the data.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' if there are no changes to the matrix
+  i <- x$getmat()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setmat(i)
+  i
 }
